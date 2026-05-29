8ff398737aa6bfbbef2a9df7bde68e4edaf0adf2
edu-kenshi <edu.kenshi@gmail.com>
2026-05-29 10:27:01 +0900

    Feat: 백엔드 기본틀

---
+++ server.py
@@ -0,0 +1,16 @@
+'''
+백엔드 프로그램
+기능
+    - ~/chat URL 제공
+    - 클라이언트 채팅입력 => ~/chat 요청 => 프럼프트구성 => bedrock 호출 => 응답 => 처리 => 프런트
+'''
+# 1. 모듈 가져오기
+from fastapi import FastAPI
+
+# 2. fastapi 객체 생성
+app = FastAPI(title='식사 메뉴 추천 AI')
+
+# 3. API 구성
+@app.post('/chat')
+async def chat():
+    return "채팅 응답"
