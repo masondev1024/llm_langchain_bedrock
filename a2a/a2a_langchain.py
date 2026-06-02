21a31860805e7a84ab6eb0df9217de4400eeb392
edu-kenshi <edu.kenshi@gmail.com>
2026-06-02 10:47:32 +0900

    Feat: 3개의 에이전트의 페스로나와 프럼프트 포멧

--- a2a/a2a_langchain.py
+++ a2a/a2a_langchain.py
@@ -10,7 +10,29 @@ import os
 dotenv.load_dotenv()
 
 # LLM 생성, 차후 에이전트별로 역활에 따라 최적의 LLM 배치 할수 있음
+# 모든 에이전트가 모두 사용. 페르소나에 따라 역활, 성능 조정할 수 있다
 llm = ChatBedrock( model_id = os.getenv('MODEL_ID'),
     client       = boto3.client('bedrock-runtime', region_name=os.getenv('AWS_REGION') ),
-    model_kwargs = {"temperature:0.7"}
+    model_kwargs = {"temperature":0.7}
 )
+
+# Agent 1, 신입 개발자를 위한 프럼프트 구성
+developer_prompt = ChatPromptTemplate.from_messages([
+    ('system', '당신은 열정적인 "신입 파이썬 개발자"입니다. 요청받은 기능을 구현하는 코드를 작성하세요. 설명은 최소화 하고 코드 위주로 작성하세요.'),
+    ('user'  , '{request}'),
+])
+# Agent 2, 전문 리뷰어를 위한 프럼프트 구성
+reviewer_prompt = ChatPromptTemplate.from_messages([
+    ('system', '''당신은 까다로운 "전문 개발자"입니다. 신입 개발자가 작성한 코드를 리뷰하세요. 
+보안 취약점, 비효율적인 부분, 스타일 가이드를 점검하고 수정 제안을 하세요.
+코드가 완벽하자면, "PASS"라고만 답하세요.
+'''
+     ),
+    ('user'  , '다음 코드를 리뷰해주세요:\n\n{code}'),
+])
+# Agent 3, (코드 리뷰를 기반으로 코드를 수정하는 )리파인더를 위한 프럼프트 구성
+refiner_prompt = ChatPromptTemplate.from_messages([
+    ('system', '당신은 열정적인 "신입 파이썬 개발자"입니다. 전문개발자의 리뷰를 보고 코드를 수정하여 다시 제출하세요.'),
+    ('user'  , '이전 코드:\n{original_code}\n\n리뷰 내용:\n{feedback}\n\n위 내용을 반영하여 개선된 전체 코드를 다시 작성하세요'),
+])
+
